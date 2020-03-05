ddd1b94f19383ee95d05019dab204f2d557c9b83
Luke Olali <mugapedia@gmail.com>
2020-03-05 17:51:16 +0300

    Replaces the Counter component with HookCounter component within the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Counter from './components/Counter';
+import HookCounter from './components/HookCounter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <HookCounter />
     </div>
   );
 }
