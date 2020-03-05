a49635a46cf00bdbe3aaffd519b547b8c2dc94e6
Luke Olali <mugapedia@gmail.com>
2020-03-05 17:40:42 +0300

    Renders the Counter component within the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Counter from './components/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Counter />
     </div>
   );
 }
