9158840a91c4de5d4b3cb974d7bce2a71798f9c9
Luke Olali <mugapedia@gmail.com>
2020-03-10 14:24:25 +0300

    Defines method to manipulate state of the HookCounterTwo component based on its previous state

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -5,7 +5,10 @@ function HookCounterTwo() {
   const [count, setCount] = useState(initialState);
   return (
     <div>
-      
+      <p>Count {count}</p>
+      <button onClick={ () => setCount(initialState) }>Reset</button>
+      <button onClick={ () => setCount(count + 1) }>Increment</button>
+      <button onClick={ () => setCount(count - 1) }>Decrement</button>
     </div>
   );
 }
