a59edb2680925efcce20bb906412da6323e6a097
Luke Olali <mugapedia@gmail.com>
2020-03-10 16:02:50 +0300

    Defines the initial state of the HookCounterFour component, adds JSX that will be rendered by the component

--- src/components/HookCounterFour.js
+++ src/components/HookCounterFour.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function HookCounterFour() {
+  const [items, setItems] = useState([]);
   return (
     <div>
-      
+        <ul>
+          {
+            items.map(currItem => <li key={currItem.id}>{currItem.value}</li>)
+          }
+        </ul>
     </div>
   )
 }
