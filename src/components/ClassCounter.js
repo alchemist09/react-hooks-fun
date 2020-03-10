3705dc1a3be4a95968683d1474dc0fbb737e8fb8
Luke Olali <mugapedia@gmail.com>
2020-03-10 16:36:12 +0300

    Declares the ClassCounter component and sets its initial state

---
+++ src/components/ClassCounter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
