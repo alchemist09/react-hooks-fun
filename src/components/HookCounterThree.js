72a9fc3180a1a1e5e15013c951a263b0da18dce1
Luke Olali <mugapedia@gmail.com>
2020-03-10 15:17:48 +0300

    Declares the state of the HookCounterThree component, adds JSX that renders a form to its return function

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function HookCounterThree() {
+  const [name, setName] = useState({ firstname: '', lastname: '' });
   return (
     <div>
-      
+      <form>
+        <input
+          type="text"
+          value={name.firstname}
+        />
+        <input
+          type="text"
+          value={name.lastname}
+        />
+      </form>
+      <p>First Name: {name.firstname}</p>
+      <p>Last Name: {name.lastname}</p>
     </div>
   )
 }
