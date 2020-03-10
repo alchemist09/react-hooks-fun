3d16781bc9b17c9862f6c02a49f3a8b21aa29b44
Luke Olali <mugapedia@gmail.com>
2020-03-10 14:33:45 +0300

    Change the increment and decrement methods to use the previous state of the HookCounterTwo component

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -7,8 +7,8 @@ function HookCounterTwo() {
     <div>
       <p>Count {count}</p>
       <button onClick={ () => setCount(initialState) }>Reset</button>
-      <button onClick={ () => setCount(count + 1) }>Increment</button>
-      <button onClick={ () => setCount(count - 1) }>Decrement</button>
+      <button onClick={ () => setCount(prevState => prevState + 1) }>Increment</button>
+      <button onClick={ () => setCount(prevState => prevState - 1) }>Decrement</button>
     </div>
   );
 }
