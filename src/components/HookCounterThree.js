9ee9fafef2d82891714ca041c0c0f92c16335c59
Luke Olali <mugapedia@gmail.com>
2020-03-10 15:28:54 +0300

    Fixes issue caused by overwritting of other state fields when state updates

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -8,12 +8,12 @@ function HookCounterThree() {
         <input
           type="text"
           value={name.firstname}
-          onChange={e => setName({ firstname: e.target.value })}
+          onChange={e => setName({ ...name, firstname: e.target.value })}
         />
         <input
           type="text"
           value={name.lastname}
-          onChange={e => setName({ lastname: e.target.value })}
+          onChange={e => setName({ ...name, lastname: e.target.value })}
         />
       </form>
       <p>First Name: {name.firstname}</p>
