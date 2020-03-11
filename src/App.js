9696951458a2307041ab18538f84b7a90a768e01
Luke Olali <mugapedia@gmail.com>
2020-03-11 14:40:49 +0300

    Renders the ClassCounter component within App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import EffectCounterOne from './components/useEffect/EffectCounter01';
 import './App.css';
+import ClassCounter from './components/useEffect/ClassCounter';
 
 function App() {
   return (
     <div className="App">
-      <EffectCounterOne />
+      <ClassCounter />
     </div>
   );
 }
