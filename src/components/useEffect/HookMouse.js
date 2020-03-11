982803f8f225436c0350344f4abe24922b25e854
Luke Olali <mugapedia@gmail.com>
2020-03-11 16:25:06 +0300

    Defines the handleMouseMove event handler that listens to mousemove events

--- src/components/useEffect/HookMouse.js
+++ src/components/useEffect/HookMouse.js
@@ -4,6 +4,11 @@ function HookMouse() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
+  const handleMouseMove = (evt) => {
+    setX(evt.clientX);
+    setY(evt.clientY);
+  }
+
   useEffect(() => {
     console.log("useEffect......mousemove event handled");
     window.addEventListener('mousemove', handleMouseMove);
