243ae66b314c43946d7497f63c708e61b7871b67
Luke Olali <mugapedia@gmail.com>
2020-03-11 15:17:27 +0300

    Creates new state variable 'name', and an input element used to update the state variable

--- src/components/useEffect/EffectCounter01.js
+++ src/components/useEffect/EffectCounter01.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function EffectCounterOne() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
 
   useEffect(() => {
     document.title = "You clicked " + count + " times";
@@ -9,6 +10,11 @@ function EffectCounterOne() {
 
   return (
     <div>
+      <input
+        type="text"
+        value={name}
+        onChange={ e => setName(e.target.value) }
+      />
       <button onClick={() => { setCount(prevState => prevState + 1) }}>Count {count} times</button>
     </div>
   )
