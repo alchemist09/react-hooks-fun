e31722aa294b630cd4d7d1f276846e0098ffe1d7
Luke Olali <mugapedia@gmail.com>
2020-03-11 16:03:32 +0300

    Declares the initial state of the ClassMouse component, adds a lifecycle hook

--- src/components/useEffect/ClassMouse.js
+++ src/components/useEffect/ClassMouse.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 class ClassMouse extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('mousemove', this.handleMouseMove);
+  }
+
   render() {
+    const { x, y } = this.state;
     return (
       <div>
-        
+        <p>X - {x} &nbsp;&nbsp; Y - {y} </p>
       </div>
     )
   }
