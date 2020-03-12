cb02a62ae6b8cb5c0780a0487d6f28e3bf31996b
Luke Olali <mugapedia@gmail.com>
2020-03-12 16:15:37 +0300

    Species the value to watch for within the dependency list of the useEffect hook

--- src/components/useEffect/IntervalHookCounter.js
+++ src/components/useEffect/IntervalHookCounter.js
@@ -4,12 +4,17 @@ function IntervalHookCounter() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const tick = () => setCount(count + 1);
+    const tick = () => {
+      console.log('tick() called................')
+      console.log("count: ", count);
+      setCount(count + 1);
+    }
+
     const interval = setInterval(tick, 1500);
     return () => {
       clearInterval(interval);
     }
-  }, [])
+  }, [count])
 
   return (
     <div style={{ width: '70%', textAlign: 'center', color: '#2a23fb' }}>
