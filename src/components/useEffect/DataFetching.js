4a788cf2913eeb8ff3281c4ead7a86a56376d936
Luke Olali <mugapedia@gmail.com>
2020-03-12 17:14:12 +0300

    Declares the DataFetching component, adds its initial state

---
+++ src/components/useEffect/DataFetching.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function DataFetching() {
+  const [posts, setPosts] = useState([]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DataFetching;
