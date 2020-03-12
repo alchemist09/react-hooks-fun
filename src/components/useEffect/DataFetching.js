91df77ba561e896e141f51f49505293a55e0f6f0
Luke Olali <mugapedia@gmail.com>
2020-03-12 18:06:14 +0300

    Refactor the DataFetching component to fetch a particular post only when a button is clicked

--- src/components/useEffect/DataFetching.js
+++ src/components/useEffect/DataFetching.js
@@ -4,19 +4,22 @@ import axios from 'axios';
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(id);
 
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    console.log("useEffect triggered.......");
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then(res => {
           console.log(res);
           setPost(res.data);
         })
       .catch(err => console.log(err));
-  }, [id]);
+  }, [idFromButtonClick]);
 
   return (
     <div>
       <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button onClick={() => setIdFromButtonClick(id)}>Fetch Post</button>
       <p>{post.title}</p>
     </div>
   )
