ff4a19f40323cfb7d6d95da1f0af9388f18b3e7a
Luke Olali <mugapedia@gmail.com>
2020-03-12 15:57:21 +0300

    Renders the IntervalHookCounter component within the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import IntervalClassCounter from './components/useEffect/IntervalClassCounter';
+import IntervalHookCounter from './components/useEffect/IntervalHookCounter';
 
 function App() {
   return (
     <div className="App">
       <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
   );
 }
