b9a9e3280c2d33aad25dea192c0b83342b77d945
Luke Olali <mugapedia@gmail.com>
2020-03-12 17:36:27 +0300

    Implement the useEffect hook to fetch data from REST API upon component mounting

--- src/components/useEffect/DataFetching.js
+++ src/components/useEffect/DataFetching.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function DataFetching() {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+          console.log(res);
+          setPosts(res.data);
+        })
+      .catch(err => console.log(err));
+  });
+
   return (
     <div>
-      
+      <ul>
+        {
+          posts.map(currPost => <li key={currPost.id}>{currPost.title}</li>)
+        }
+      </ul>
     </div>
   )
 }
