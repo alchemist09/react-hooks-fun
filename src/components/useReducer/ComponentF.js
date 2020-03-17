4626aad46622182d508eba1b5e822d1e346d5074
Luke Olali <mugapedia@gmail.com>
2020-03-17 18:50:22 +0300

    Manipulates the global state from within ComponentF by the help of the useContext hook

--- src/components/useReducer/ComponentF.js
+++ src/components/useReducer/ComponentF.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CounterContext } from '../../App'
 
 function ComponentF() {
+  const { counterDispatch } = useContext(CounterContext)
+
   return (
     <div>
-      <p>Component F</p>
+      <span>Component F</span>
+      <button onClick={ () => counterDispatch('increment') }>Increment</button>
+      <button onClick={ () => counterDispatch('decrement') }>Decrement</button>
+      <button onClick={ () => counterDispatch('reset') }>Reset</button>
     </div>
   )
 }
