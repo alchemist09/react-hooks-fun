c8f53366677786cb2acedfbd012eecf469f6a417
Luke Olali <mugapedia@gmail.com>
2020-03-17 11:26:03 +0300

    Declares component acts as an intermediate component across which props are supposed to pass through implicitly

---
+++ src/components/useContext/RandomClass.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ClassContext from './ClassContext';
+
+class RandomClass extends Component {
+  render() {
+    return (
+      <ClassContext />
+    )
+  }
+}
+
+export default RandomClass;
