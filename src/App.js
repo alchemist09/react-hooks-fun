e8f73bedf8619be2ea1f0ac7ba283416e2378d40
Luke Olali <mugapedia@gmail.com>
2020-03-17 11:45:29 +0300

    Define the ThemeContext and provides at the top level of the component tree

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import RandomClass from './components/useContext/RandomClass';
 
 export const LangContext = React.createContext();
 export const UserContext = React.createContext();
+export const ThemeContext = React.createContext();
 
 function App() {
   return (
     <LangContext.Provider value="American English">
       <UserContext.Provider value="Super User">
-        <div className="App">
-          <RandomClass />
-        </div>
+        <ThemeContext.Provider value="Dark Mode">
+          <div className="App">
+            <RandomClass />
+          </div>
+        </ThemeContext.Provider>
       </UserContext.Provider>
     </LangContext.Provider>
   );
