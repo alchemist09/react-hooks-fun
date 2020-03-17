8fc147c9903f5d74487beb489683cb6909be9f91
Luke Olali <mugapedia@gmail.com>
2020-03-17 11:58:35 +0300

    Declares the HookContext component

---
+++ src/components/useContext/HookContext.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HookContext() {
+  return (
+    <div>
+      <p>Language:</p>
+      <p>User: </p>
+      <p>Theme:</p>
+    </div>
+  )
+}
+
+export default HookContext
