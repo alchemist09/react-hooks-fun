0ba342945bd88fe05eca89480ef9c7f156eda15a
Luke Olali <mugapedia@gmail.com>
2020-03-17 11:40:05 +0300

    Defines the UserContext and provides its value at the top level of the component tree

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import RandomClass from './components/useContext/RandomClass';
 
 export const LangContext = React.createContext();
+export const UserContext = React.createContext();
 
 function App() {
   return (
     <LangContext.Provider value="American English">
-      <div className="App">
-        <RandomClass />
-      </div>
+      <UserContext.Provider value="Super User">
+        <div className="App">
+          <RandomClass />
+        </div>
+      </UserContext.Provider>
     </LangContext.Provider>
   );
 }
