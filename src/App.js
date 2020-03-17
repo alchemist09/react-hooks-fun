5271b30738b0412b73fef9453a57da1785500cfb
Luke Olali <mugapedia@gmail.com>
2020-03-17 18:23:30 +0300

    Defines the reducer function that mutates global state, declares the initial state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import ComponentA from './components/useReducer/ComponentA';
 import ComponentB from './components/useReducer/ComponentB';
 import ComponentC from './components/useReducer/ComponentC';
 
+const initialState = 0;
+
+const reducer = (state = initialState, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 function App() {
   return (
     <div>
