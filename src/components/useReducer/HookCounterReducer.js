5a7fc1da7a17ac70152ccb631b86fd2552f4ee29
Luke Olali <mugapedia@gmail.com>
2020-03-17 15:44:50 +0300

    Defines the reducer function used to compute component state

--- src/components/useReducer/HookCounterReducer.js
+++ src/components/useReducer/HookCounterReducer.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+const initialState = 0
+
+const reducer = (state = initialState, action) => {
+  switch (action) {
+    case 'incement':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 function HookCounterReducer() {
   return (
-    <div style={{ width: '70%', margin: '20px auto 60px'}}>
+    <div style={{ width: '70%', margin: '20px auto 60px' }}>
       <button>Increment</button>
       <button>Decrement</button>
       <button>Reset</button>
