82b20e150946473a62fce74f3a1616ae841e1493
Luke Olali <mugapedia@gmail.com>
2020-03-17 16:40:39 +0300

    Initializes the HookCounterReducer component from the final state of HookCounterReducer component

---
+++ src/components/useReducer/HookCounterReducerComplex.js
@@ -0,0 +1,31 @@
+import React, { useReducer } from 'react'
+
+const initialState = 0
+
+const reducer = (state = initialState, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
+function HookCounterReducerComplex() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div style={{ width: '70%', margin: '20px auto 60px' }}>
+      <p>{state}</p>
+      <button onClick={ () => dispatch('increment') }>Increment</button>
+      <button onClick={ () => dispatch('decrement') }>Decrement</button>
+      <button onClick={ () => dispatch('reset') }>Reset</button>
+    </div>
+  )
+}
+
+export default HookCounterReducerComplex
