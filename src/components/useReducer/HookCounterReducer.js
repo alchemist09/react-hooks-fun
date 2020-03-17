ca2948453f7fe65df9d36984e1cb69908a311d4a
Luke Olali <mugapedia@gmail.com>
2020-03-17 15:41:58 +0300

    Declares the HookCounterReducer component

---
+++ src/components/useReducer/HookCounterReducer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HookCounterReducer() {
+  return (
+    <div style={{ width: '70%', margin: '20px auto 60px'}}>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </div>
+  )
+}
+
+export default HookCounterReducer
