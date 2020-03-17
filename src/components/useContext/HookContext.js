4596e8d62af00bfdf34fec3bdd06c3c91e386900
Luke Olali <mugapedia@gmail.com>
2020-03-17 12:01:57 +0300

    Consumes values from LangContext, UserContext, ThemeContext using the useContext hook

--- src/components/useContext/HookContext.js
+++ src/components/useContext/HookContext.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { LangContext, UserContext, ThemeContext } from '../../App'
 
 function HookContext() {
+  const language = useContext(LangContext)
+  const user = useContext(UserContext)
+  const theme = useContext(ThemeContext)
+
   return (
     <div>
-      <p>Language:</p>
-      <p>User: </p>
-      <p>Theme:</p>
+      <p>Language: {language}</p>
+      <p>User: {user}</p>
+      <p>Theme: {theme}</p>
     </div>
   )
 }
