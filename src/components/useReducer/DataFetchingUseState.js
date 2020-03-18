b72f7eb2ef05457e008e48e3216c95ba28d9d7a7
Luke Olali <mugapedia@gmail.com>
2020-03-18 11:57:48 +0300

    Defines the useEffect hook implmentation used to fetch data from an API endpoint

--- src/components/useReducer/DataFetchingUseState.js
+++ src/components/useReducer/DataFetchingUseState.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const API_ENDPOINT = `https://jsonplaceholder.typicode.com/posts/1`;
 
 function DataFetchingUseState() {
   const [loading, setLoading] = useState(true)
   const [post, setPost] = useState({})
   const [error, setError] = useState('')
-  
+
+  useEffect(() => {
+    axios.get(API_ENDPOINT)
+      .then(res => {
+        setLoading(false);
+        setPost(res.data.title)
+        setError('')
+      })
+      .catch(error => {
+        setError('Request could not be completed successfully. Something went wrong')
+      })
+  }, [])
+
   return (
     <div>
       
