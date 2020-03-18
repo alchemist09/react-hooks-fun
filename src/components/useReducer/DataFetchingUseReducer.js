37699eccb783d9d3bc48bc52815c3c7d6f0bd017
Luke Olali <mugapedia@gmail.com>
2020-03-18 12:53:46 +0300

    Defines the useEffect callback used to fetch data from API endpoint and dispatch actions to the reducer function

--- src/components/useReducer/DataFetchingUseReducer.js
+++ src/components/useReducer/DataFetchingUseReducer.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react'
+import axios from 'axios';
+
+const API_ENDPOINT = `https://jsonplaceholder.typicode.com/posts/1`
 
 const initialState = {
   loading: true,
@@ -24,6 +27,23 @@ const reducer = (state = initialState, action) => {
 }
 
 function DataFetchingUseReducer() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    axios.get(API_ENDPOINT)
+      .then(res => {
+        dispatch({
+          type: 'FETCH_SUCCESS',
+          payload: res.data
+        })
+      })
+      .catch(error => {
+        dispatch({
+          type: 'FETCH_ERROR'
+        })
+      })
+  }, [])
+
   return (
     <div>
       
