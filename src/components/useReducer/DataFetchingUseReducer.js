e73ef7016c35542b5ca6ab0c7397d6499d712479
Luke Olali <mugapedia@gmail.com>
2020-03-18 12:42:52 +0300

    Declares the DataFetchiingUseReducer compoonent and sets its initial state

---
+++ src/components/useReducer/DataFetchingUseReducer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const initialState = {
+  loading: true,
+  post: {},
+  error: ''
+}
+
+function DataFetchingUseReducer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DataFetchingUseReducer
