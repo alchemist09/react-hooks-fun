2aa55686b32a57fdb21ca261934f5e41d2c79989
Luke Olali <mugapedia@gmail.com>
2020-03-18 12:46:27 +0300

    Defines the reducer function that is used to mutate state of the component

--- src/components/useReducer/DataFetchingUseReducer.js
+++ src/components/useReducer/DataFetchingUseReducer.js
@@ -6,6 +6,23 @@ const initialState = {
   error: ''
 }
 
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS':
+      return {
+        loading: false,
+        post: action.payload
+      }
+    case 'FETCH_ERROR':
+      return {
+        loading: false,
+        error: 'Request failed. Something went wrong'
+      }
+    default:
+      return state
+  }
+}
+
 function DataFetchingUseReducer() {
   return (
     <div>
