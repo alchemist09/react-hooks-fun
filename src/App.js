179d866f3cac76eb11a74b5c1788a463cae50f3a
Luke Olali <mugapedia@gmail.com>
2020-03-18 11:37:54 +0300

    Renders the TopLevelComponent which contains the global state for the app which was previously on App component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React, { useReducer } from 'react';
-import ComponentA from './components/useReducer/ComponentA';
-import ComponentB from './components/useReducer/ComponentB';
-import ComponentC from './components/useReducer/ComponentC';
-
-export const CounterContext = React.createContext();
-
-const initialState = 0;
-
-const reducer = (state = initialState, action) => {
-  switch (action) {
-    case 'increment':
-      return state + 1
-    case 'decrement':
-      return state - 1
-    case 'reset':
-      return initialState
-    default:
-      return state
-  }
-}
+import React from 'react';
+import TopLevelComponent from './TopLevel';
 
 function App() {
-  const [countValue, dispatch] = useReducer(reducer, initialState)
-
   return (
     <div>
-      <CounterContext.Provider value={{ count: countValue, counterDispatch: dispatch }}>
-        <p>Count Value: {countValue}</p>
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </CounterContext.Provider>
+      <TopLevelComponent />
     </div>
   );
 }
