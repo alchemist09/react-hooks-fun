1d32abdc312d0a7d12e528c973ee9ec4faf28b14
Luke Olali <mugapedia@gmail.com>
2020-03-18 11:52:09 +0300

    Declares the DataFetchingUseState component and initializes its state variables

---
+++ src/components/useReducer/DataFetchingUseState.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function DataFetchingUseState() {
+  const [loading, setLoading] = useState(true)
+  const [post, setPost] = useState({})
+  const [error, setError] = useState('')
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DataFetchingUseState
