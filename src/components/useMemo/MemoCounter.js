261b34c3b02e9a4d56a1199a6afb0d4c1001e01d
Luke Olali <mugapedia@gmail.com>
2020-03-19 16:25:14 +0300

    Declares the MemoCounter component and initializes its state

---
+++ src/components/useMemo/MemoCounter.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function MemoCounter() {
+  const [counterOne, setCounterOne] = useState(0)
+  const [counterTwo, setCounterTwo] = useState(0)
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MemoCounter
