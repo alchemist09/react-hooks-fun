6b65b66240d2c4b71025d25a90cb8488287d1ebc
Luke Olali <mugapedia@gmail.com>
2020-03-19 10:35:29 +0300

    Declares the ParentComponent and initializes its state variables

---
+++ src/components/useCallback/ParentComponent.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function ParentComponent() {
+  const [age, setAge] = useState(18)
+  const [salary, setSalary] = useState(10000)
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ParentComponent
