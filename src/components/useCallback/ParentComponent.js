9e5e750b095bf881059bf0c4b1020ab8c166b464
Luke Olali <mugapedia@gmail.com>
2020-03-19 11:11:04 +0300

    Defines the methods used to update the age and salary state variables of the ParentComponent

--- src/components/useCallback/ParentComponent.js
+++ src/components/useCallback/ParentComponent.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 function ParentComponent() {
   const [age, setAge] = useState(18)
   const [salary, setSalary] = useState(10000)
+
+  setAge = () => age + 1
+
+  setSalary = () => salary + 1280
   
   return (
     <div>
