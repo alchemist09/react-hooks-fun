cc5c2998251767f8c10d936dd08037a9b36ee349
Luke Olali <mugapedia@gmail.com>
2020-03-19 16:32:54 +0300

    Defines the state mutation functions for the state variables and passes them as callbacks to the click event registered on the button elements

--- src/components/useMemo/MemoCounter.js
+++ src/components/useMemo/MemoCounter.js
@@ -3,10 +3,23 @@ import React, { useState } from 'react'
 function MemoCounter() {
   const [counterOne, setCounterOne] = useState(0)
   const [counterTwo, setCounterTwo] = useState(0)
-  
+
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1)
+  }
+
+  const incrementTwo = () => {
+    setCounterTwo(counterTwo + 1)
+  }
+
   return (
     <div>
-      
+      <p>
+        <button onClick={incrementOne}>Counter One: {counterOne}</button>
+      </p>
+      <p>
+        <button onClick={incrementTwo}>Counter Two: {counterTwo}</button>
+      </p>
     </div>
   )
 }
