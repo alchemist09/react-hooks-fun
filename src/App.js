f26efd9b0e84e0b7d669ffd4137df65d5b338b85
Luke Olali <mugapedia@gmail.com>
2020-03-22 19:29:42 +0300

    Renders the CounterTwo component within App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CounterOne from './components/CustomHooks/CounterOne';
+import CounterTwo from './components/CustomHooks/CounterTwo';
 
 function App() {
   return (
     <div >
       <CounterOne />
+      <CounterTwo />
     </div>
   );
 }
