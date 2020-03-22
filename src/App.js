3d79a78923355dab0ea67f2d24e62b74e30c18ce
Luke Olali <mugapedia@gmail.com>
2020-03-22 19:10:03 +0300

    Renders the DocTitleTwo component within the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DocTitleOne from './components/CustomHooks/DocTitleOne';
+import DocTitleTwo from './components/CustomHooks/DocTitleTwo';
 
 function App() {
   return (
     <div >
       <DocTitleOne />
+      <DocTitleTwo />
     </div>
   );
 }
