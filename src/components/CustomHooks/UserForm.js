66de852eaa86a0244fd5262ae11c2461c4d13e17
Luke Olali <mugapedia@gmail.com>
2020-03-22 20:15:54 +0300

    Defines the state variables for the UseForm component and registers handlers on the form input elements to mutate those values

--- src/components/CustomHooks/UserForm.js
+++ src/components/CustomHooks/UserForm.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UserForm() {
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+
   return (
     <div style={{ width: '60%', margin: '1em auto 4em' }}>
       <form>
         <p>
           <label>First Name</label>
-          <input type="text" />
+          <input type="text" value={firstname} onClick={ e => setFirstname(e.target.value) } />
         </p>
         <p>
           <label>Last Name</label>
-          <input type="text" />
+          <input type="text"  value={lastname} onClick={ e => setLastname(e.target.value) } />
         </p>
         <button>Submit</button>
       </form>
