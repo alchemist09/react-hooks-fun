f0116c471d395748b5417289595b2196b1dc300b
Luke Olali <mugapedia@gmail.com>
2020-03-22 15:34:23 +0300

    Uses the useRef hook to focus the input element when InputFocus component loads

--- src/components/useRef/InputFocus.js
+++ src/components/useRef/InputFocus.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function InputFocus() {
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
   return (
     <div style={{ width: '60%', margin: '20px auto 20px 100px'}}>
-      <input type="text" />
+      <input ref={inputRef} type="text" />
     </div>
   )
 }
