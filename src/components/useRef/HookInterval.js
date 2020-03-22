466cd0dcd9d7cdd0ed8a7517ae4e80dd8f2d50f2
Luke Olali <mugapedia@gmail.com>
2020-03-22 17:31:22 +0300

    Defines the lifecycle hook used to mutate the state of the timer variable

--- src/components/useRef/HookInterval.js
+++ src/components/useRef/HookInterval.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function HookInterval() {
   const [timer, setTimer] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(prevTimer => prevTimer + 1)
+    }, 1200)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <div style={{ width: '50%', margin: '0.4em auto 4em'}}>
